a8e43caeb109bcdc9e73501c665290ca4ceb1737
Chris Cranford <chris@hibernate.org>
2025-09-29 15:33:59 -0400

    Update to Docusaurus 3.9.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,12 @@ const config = {
   projectName: 'godot-orchestrator',
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn'
+    }
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
